d0404ef9d7c9e8671927a582cb53f8de31c3687a
lahib-chowdhury <lahibrahman@gmail.com>
2025-09-19 21:53:27 +0600

    Add formatting_string.py with f-string examples

---
+++ formating_string.py
@@ -0,0 +1,8 @@
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+age = 20
+text = f"it is {age} years old"
+print(text)
+size = 59
+txt = f"The size is {size:.2f} inches"
+print(txt)
---
+++ formatting_string.py
@@ -0,0 +1,1 @@
+txt = f"The price is {20 * 59} dollars"\nprint(txt)\n\nage = 20\ntext = f"it is {age} years old"\nprint(text)\n\nsize = 59\ntxt = f"The size is {size:.2f} inches"\nprint(txt)
